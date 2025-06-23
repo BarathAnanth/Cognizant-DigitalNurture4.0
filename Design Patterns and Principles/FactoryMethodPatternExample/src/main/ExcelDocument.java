61b395a97b54c8b997e2d565e9deba2d89f73d07
Barath Ananth <142615556+BarathAnanth@users.noreply.github.com>
2025-06-23 13:55:58 +0530

    Delete Design Patterns and Principles directory

--- Design Patterns and Principles/FactoryMethodPatternExample/src/main/ExcelDocument.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-public class ExcelDocument implements Document {
-    public void open() {
-        System.out.println("Opening Excel Document");
-    }
-}
--- Design Patterns and Principles/FactoryMethodPatternExample/src/main/PdfDocument.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-public class PdfDocument implements Document {
-    public void open() {
-        System.out.println("Opening PDF Document");
-    }
-}
